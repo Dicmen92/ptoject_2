1a0412b3f906739ec4e26d1d8c79481c6c9ab09c
Dmitriy Babich <babich-d@mail.ru>
2020-09-11 00:13:18 +0600

    git commit -m

--- js/index.js
+++ js/index.js
@@ -300,7 +300,7 @@ window.addEventListener('DOMContentLoaded', function(){
 dataImage();
 
 //Запрет на ввод симфолов кроме цифр
-const calc = () => {  
+const calcNum = () => {  
   const calcBlock = document.querySelectorAll('.calc-block>input');
     calcBlock.forEach((item) => {      
       item.addEventListener('input', () => {
@@ -308,6 +308,52 @@ const calc = () => {
       })
     }); 
 };
-calc();
+calcNum();
+
+//калькулятор
+const calc = (price = 100) => {
+  const calcBlock = document.querySelector('.calc-block'),
+        calcType = document.querySelector('.calc-type'),
+        calcSquare = document.querySelector('.calc-square'),
+        calcCount = document.querySelector('.calc-count'),
+        calcDay = document.querySelector('.calc-day'),
+        totalValue = document.getElementById('total');
+
+  const countSum = () => {
+    let total = 0,
+    countValue = 1,
+    dayValue = 1;
+    const typeValue = calcType.options[calcType.selectedIndex].value,
+    squareValue = +calcSquare.value;
+
+    if (calcCount.value > 1) {
+      countValue += (calcCount.value - 1) / 10;
+    }
+
+    if (calcDay.value && calcDay.value < 5) {
+      dayValue *= 2;
+    } else if (calcDay.value && calcDay.value < 10){
+      dayValue *= 1.5;
+    }
+
+    if (typeValue && squareValue) {
+      total = price * typeValue * squareValue * countValue * dayValue;
+    }
+
+    totalValue.textContent = total
+    
+  };
+
+  calcBlock.addEventListener('change', (event) => {
+    const target = event.target;
+
+    if (target.matches('select') || target.matches('input')) {
+      countSum();      
+    }
+
+  });
+
+};
+calc(100);
 
 });
